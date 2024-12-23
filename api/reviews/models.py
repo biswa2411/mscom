43c1b12391fdc5e3255072aa02dc0162ba48494d
TUSHAR TUSHAR <biswajitroutray58@gmail.com>
2024-12-23 14:37:51 +0530

    updated the reviews modek for fixing the migration issue

--- api/reviews/models.py
+++ api/reviews/models.py
@@ -4,7 +4,7 @@ from products.models import Product
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     rating = models.IntegerField()
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
